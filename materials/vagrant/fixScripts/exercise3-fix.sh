8d3fa3082df5df0eb83107b552522325b972bb49
issa.daniel <issa.daniel@imperva.com>
2021-12-08 00:39:46 +0000

    solution from Issa.Daniel

--- materials/vagrant/fixScripts/exercise3-fix.sh
+++ materials/vagrant/fixScripts/exercise3-fix.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-#add fix to exercise3 here
+sed -i 's/all denied/all granted/g' /etc/apache2/sites-available/000-default.conf
+systemctl restart apache2.service
--- materials/vagrant/fixScripts/exercise6-fix.sh
+++ materials/vagrant/fixScripts/exercise6-fix.sh
@@ -1,2 +1,27 @@
 #!/bin/bash
-#add fix to exercise6-fix here
+
+DESTINATION_FOLDER=${@: -1}
+DESTINATION_SERVER=server2
+SIZE=0
+
+#check if we are login into server2 and change the DESTINATION_SERVER to server1
+if [ "$(hostname)" == "server2" ]
+then
+        DESTINATION_SERVER=server1
+fi
+
+# copy files and get the files size
+for file in ${@:1:$#-1}
+do
+  scp -qi /home/vagrant/.ssh/id_rsa $file vagrant@$DESTINATION_SERVER:$DESTINATION_FOLDER
+  SIZE=$((${SIZE}+$(du -b $file | awk '{print$1}')))
+done
+
+
+# print size
+if [ $? -eq 0 ]
+then
+       echo $SIZE
+else
+       echo "Error, ${@:1:$#-1} could not tranfer to $DESTINATION_SERVER"
+fi
